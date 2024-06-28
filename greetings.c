2a3c57b043b52736e150a5099e564cbbe16de5af
JinnyLyn <noh8829@protonmail.com>
2024-06-28 23:25:12 +0900

    added dirs

--- greetings.c
+++ greetings.c
@@ -1,6 +1,27 @@
 #include <stdio.h>
 
+void clrInputBuffer() {
+  char c;
+  while(c = getchar() != '\0' && EOF);
+}
+
+
+void getName(char *name, int check) {
+  if (check != 1) {
+    printf("E -1\n");
+    return -1;
+  }
+
+  char name = (char*)malloc(sizeof(char) * );
+  while(1) {
+    clrInputBuffer();
+    printf("Your name: ");
+    
+  }
+
+  return *name;
+}
+
 int main() {
-  printf("반갑다 연호야!!!!");
-  return 0;
+
 }

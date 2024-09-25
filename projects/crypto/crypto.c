945097fbc57b4c8d31a79dde505eb9f83cc3aa6c
JinnyLyn <noh8829@protonmail.com>
2024-09-26 03:54:39 +0900

    refactured main and other function structure.

--- projects/crypto/crypto.c
+++ projects/crypto/crypto.c
@@ -5,6 +5,7 @@
 
 /**
  * *return codes:
+ * * 0: successfully finished task
  * *-1: error reading input
  * *-2: memory allocation failure
  */
@@ -21,10 +22,16 @@ void InitialMessage() {
   printf("\n\n");
 }
 
+/* Frees allocated memory for variables.
+ * Needs to be added each time a new memory is allocaed by malloc.*/
+void free_Malloc_variables() {
+  free(object_string);
+  free(result_data);
+}
 
 //*인풋값 유효여부 확인(아직 안쓰임). 문제있으면 -1 돌려줌.
 int validateInput(int min, int max) {
-  char input[10];
+  char input[sizeof(int)];
   int value;
 
   printf("Please type your selection (%d - %d)\n", min, max);
@@ -56,10 +63,10 @@ void clrInputBuffer() {
  * Encryption function. 암호화 기능 함수.
  * @param method Parameter for this function will determine the encryption method.
  */
-int encrypting(int method) {
+int encrypting(int method, char* toEncrypt, char* result) {
   switch (method) {
     case 1:
-
+   //TODO: start here!! First Create Your OWN Encryption Method that can be done other way!!
   }
 }
 
@@ -80,8 +87,11 @@ int main(void) {
   int decrypt_method = 0;
   int encrypt_method = 0;
   char * object_string = (char *)malloc(sizeof(char) * 100);
-  if (object_string == NULL) {
+  char * result_data = (char *)malloc(sizeof(char) * 512);
+  if (object_string == NULL || result_data == NULL) {
     printf("Memory allocation failed!\nExiting...\n");
+    
+    free_Malloc_variables();
     return -2;
   }
   //print out initial message
@@ -90,11 +100,13 @@ int main(void) {
 
   printf("This is a basic decrypt/encrypt program.\n");
   printf("What has been encrypted using this program is only decrypt-able with this one. (Ideally...)\n");
+  printf("This program only works with English Alphabets on current version!");
   printf("Please enter what you want to encrypt: \n");
   if (fgets(object_string, sizeof(object_string), stdin)!= NULL) {
     object_string[strcspn(object_string, "\n")] = '\0';
   } else {
     printf("Error reading input.\nExiting...\n");
+    free_Malloc_variables();
     return -1;
   }
 
@@ -107,20 +119,20 @@ int main(void) {
 
     switch (decrypt_or_encrypt) {
       case 1: //암호화
-        printf("Select encrypting method: \n");
+        printf("Select encrypting method(#1 will be based on my own algorithm.): \n");
         //암호화 방법 몇가지(baseu64 url 등등) 사용해보기.
-        printf("[1]asdf\n[2]asdf\n[3]asdf\n[4]asdf\n[5]asdf\n");
+        printf("[1]CIPHER\n[2]asdf\n[3]asdf\n[4]asdf\n[5]asdf\n");
         encrypt_method = validateInput(1, 5); //함수 입력값은 선택지에 따라 바뀜
         if(encrypt_method != -1) {
-          encrypting(encrypt_method);
+          encrypting(1, object_string, result_data);
           //계속해
         }
       break;
 
       case 2: //복호화
-        printf("Select decrypting method: \n");
+        printf("Select decrypting method(use #1 if you used this program to encrypt.): \n");
         //복호화 방법 몇가지 사용해보기.
-        printf("[1]asdf\n[2]asdf\n[3]asdf\n[4]asdf\n[5]asdf\n");
+        printf("[1]DCIPHER\n[2]asdf\n[3]asdf\n[4]asdf\n[5]asdf\n");
         decrypt_method = validateInput(1, 5); //함수 입력값은 선택지에 따라 바뀜
         if(decrypt_method != -1) {
           decrypting(decrypt_method);
@@ -130,6 +142,12 @@ int main(void) {
     }
   } else {
     printf("Invalid input. Exiting...\n");
+    free_Malloc_variables();
     return -1;
   }
+
+  printf("Here is the Result!\n");
+  printf("[%f]\n");
+  free_Malloc_variables();
+  return 0;
 }

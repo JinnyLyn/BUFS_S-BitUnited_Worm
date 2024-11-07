e7ba054ad32eaaef4915b3f0cd2f372647b970e6
JinnyLyn <noh8829@protonmail.com>
2024-11-07 17:45:13 +0900

    just testing

--- j진서/fgetsusage.c
+++ j진서/fgetsusage.c
@@ -2,20 +2,22 @@
 #include <string.h>
 
 void clrInputBuffer() {
-    int c;
-    while((c = getchar())!= '\n' && c!= EOF);
+  int c;
+  while ((c = getchar()) != '\n' && c != EOF)
+    ;
 }
 
 int main() {
-    char input[100];
+  char input[100];
 
-    printf("Enter a string: ");
-    if (fgets(input, sizeof(input), stdin)) {
-        input[strcspn(input, "\n")] = '\0'; // Remove newline character
-        printf("You entered: %s\n", input);
-    } else {
-        printf("Error reading input.\n");
-    }
+  printf("Enter a string: ");
+  if (fgets(input, sizeof(input), stdin)) {
+    input[strcspn(input, "\n")] = '\0'; // Remove newline character
+    printf("You entered: %s\n", input);
+  } else {
+    printf("Error reading input.\n");
+  }
+  clrInputBuffer();
 
-    return 0;
+  return 0;
 }

